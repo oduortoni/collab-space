8f0647f3622f5a26410d665f209119ff65f4c844
oduortoni <oduortoni@gmail.com>
2025-08-09 21:27:59 +0300

    feat: add integration tests for Project functionality

---
+++ cospace/tests/Integration/ProjectIntergrationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Integration;
+
+use Tests\TestCase;
+
+class ProjectIntegrationTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
