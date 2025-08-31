54556c8ae25f0b2785900ee47f4904cb030d6569
oduortoni <oduortoni@gmail.com>
2025-08-31 11:13:22 +0300

    feat: add routes for projects

--- cospace/routes/project/web.php
+++ cospace/routes/project/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Project\ProjectController;
+use App\Http\Controllers\Project\ProjectMemberController;
+use App\Http\Controllers\Project\ProjectChangeRequestController;
+use App\Http\Controllers\Project\ProjectAuditLogController;
 
 Route::prefix('projects')
     ->name('projects.')
@@ -15,6 +18,21 @@ Route::prefix('projects')
             Route::get('/{id}/edit', [ProjectController::class, 'edit'])->name('edit');
             Route::put('/{id}', [ProjectController::class, 'update'])->name('update');
             Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('destroy');
+            
+            // Member management routes
+            Route::get('/{project}/members', [ProjectMemberController::class, 'index'])->name('members.index');
+            Route::post('/{project}/members', [ProjectMemberController::class, 'store'])->name('members.store');
+            Route::put('/{project}/members/{member}', [ProjectMemberController::class, 'update'])->name('members.update');
+            Route::delete('/{project}/members/{member}', [ProjectMemberController::class, 'destroy'])->name('members.destroy');
+            
+            // Change request routes
+            Route::get('/{project}/change-requests', [ProjectChangeRequestController::class, 'index'])->name('change-requests.index');
+            Route::post('/{project}/change-requests', [ProjectChangeRequestController::class, 'store'])->name('change-requests.store');
+            Route::post('/{project}/change-requests/{changeRequest}/approve', [ProjectChangeRequestController::class, 'approve'])->name('change-requests.approve');
+            Route::post('/{project}/change-requests/{changeRequest}/reject', [ProjectChangeRequestController::class, 'reject'])->name('change-requests.reject');
+            
+            // Audit log routes
+            Route::get('/{project}/audit-logs', [ProjectAuditLogController::class, 'index'])->name('audit-logs.index');
         });
         
         Route::get('/{id}', [ProjectController::class, 'show'])->name('show');
